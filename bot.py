43b284fe214eca73e864426025b32bb740bea490
Alexandre Yuri <48872044+virtusplmkrs@users.noreply.github.com>
2020-06-04 11:46:33 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,47 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                         RegexHandler, ConversationHandler)
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+
+STATE1 = 1
+STATE2 = 2
 
 def welcome(update, context):
     message = "Olá, " + update.message.from_user.first_name + "!"
     print(message)
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+def especialidades(update, context):
+    message = '''Por favor, que informação você deseja? \n
+            1 - Automação \n
+            2 - Programação'''
+    update.message.reply_text(message, reply_markup=ReplyKeyboardMarkup([], one_time_keyboard=True)) 
+    return STATE1
+
+def inputEspecialidade(update, context):
+    especialidade = lower(update.message.text)
+    print(especialidade)
+    if especialidade == '''1' or 'automação'
+            or especialidade == 'arduino'
+            or especialidade == 'raspbarry''':
+        message = '''https://youtu.be/njq1_fANhzY \n
+                https://youtu.be/ju8-27eRHXM'''
+        context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+        return STATE2
+    else:
+        if especialidade == '''2' or 'programação'
+            or especialidade == 'python'
+            or especialidade == 'php''':
+            message = '''https://youtu.be/3Uzbn2UoPjs'''
+            context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+            return STATE2
+
+
+def inputAssunto(update, context):
+    message = "Muito obrigada em breve teremos novos temas!"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+def cancel(update, context):
+    return ConversationHandler.END
 
 def main():
     token   = '1162143286:AAFp6iw1Apc7aahFmQSKT80OosgFtU46pkg'
@@ -11,6 +49,15 @@ def main():
     
     updater.dispatcher.add_handler(CommandHandler('start', welcome))
 
+    conversation_handler = ConversationHandler(
+        entry_points=[CommandHandler('especialidades', especialidades)],
+        states={
+            STATE1: [MessageHandler(Filters.text, inputEspecialidade)],
+            STATE2: [MessageHandler(Filters.text, inputAssunto)]
+        },
+        fallbacks=[CommandHandler('cancel', cancel)])
+    updater.dispatcher.add_handler(conversation_handler)
+    
     updater.start_polling()
     print(str('Olá, eu sou o Virtus' + updater))
     updater.idle()
